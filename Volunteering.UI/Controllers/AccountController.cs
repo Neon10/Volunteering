ed8dbaecb230dd4d0ecaa6c7615d022ead42bc07
Aziz Marzouki <mohamedaziz.marzouki@esprit.tn>
2018-05-25 05:28:24 +0100

    improved userService, refactored RegisterUser, added index images slider.

--- Volunteering.UI/Controllers/AccountController.cs
+++ Volunteering.UI/Controllers/AccountController.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
-using Microsoft.Owin.Security;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Mvc;
-using Volunteering.Domain.Entities;
-using Volunteering.Domain.Enums;
 using Volunteering.Service.Identity;
 using Volunteering.UI.Models;
 
@@ -18,45 +14,10 @@ namespace Volunteering.UI.Controllers
     public class AccountController : Controller
     {
         //===========================//
-        //--------- MANAGERS ----------
+        //--------- INIT ----------
         //===========================//
 
-        private ApplicationSignInManager _signInManager;
-        private ApplicationUserManager _userManager;
         private readonly UserService _userService = new UserService();
-
-
-        public ApplicationSignInManager SignInManager
-        {
-            get
-            {
-                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
-            }
-            private set
-            {
-                _signInManager = value;
-            }
-        }
-
-        public ApplicationUserManager UserManager
-        {
-            get
-            {
-                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
-            }
-            private set
-            {
-                _userManager = value;
-            }
-        }
-
-        private IAuthenticationManager AuthenticationManager
-        {
-            get
-            {
-                return HttpContext.GetOwinContext().Authentication;
-            }
-        }
 
 
         //===========================//
@@ -85,7 +46,7 @@ namespace Volunteering.UI.Controllers
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await _userService.SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -110,7 +71,16 @@ namespace Volunteering.UI.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult LogOff()
         {
-            AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+            _userService.AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+            return RedirectToAction("Index", "Home");
+        }
+
+        //
+        // GET: /Account/Logout
+        [Authorize]
+        public ActionResult Logout()
+        {
+            _userService.AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
             return RedirectToAction("Index", "Home");
         }
 
@@ -131,61 +101,34 @@ namespace Volunteering.UI.Controllers
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Register(AccountViewModels.RegisterViewModel model)
+        public ActionResult Register(AccountViewModels.RegisterViewModel model)
         {
             if (ModelState.IsValid)
             {
 
-                IdentityResult result;
+                IdentityResult result = new IdentityResult();
 
-                // Switch on Selected Account type
-                switch (model.AccountType)
+                _userService.RegisterUser(model.Email, model.Password, model.AccountType, ref result);
+
+                AddErrors(result);
+
+                if (result.Succeeded)
                 {
-                    // Volunteer Account type selected:
-                    case EAccountType.Volunteer:
-                        {
-                            // create new volunteer and map form values to the instance
-                            Volunteer v = new Volunteer { UserName = model.Email, Email = model.Email };
-                            result = await UserManager.CreateAsync(v, model.Password);
-
-                            // Add volunteer role to the new User
-                            if (result.Succeeded)
-                            {
-                                UserManager.AddToRole(v.Id, EAccountType.Volunteer.ToString());
-                                await SignInManager.SignInAsync(v, isPersistent: false, rememberBrowser: false);
-                                // Email confirmation here
-
-                                return RedirectToAction("Index", "Home");
-                            }
-                            AddErrors(result);
-                        }
-                        break;
-
-                    // Ngo Account type selected:
-                    case EAccountType.Ngo:
-                        {
-                            // create new Ngo and map form values to the instance
-                            Ngo ngo = new Ngo { UserName = model.Email, Email = model.Email };
-                            result = await UserManager.CreateAsync(ngo, model.Password);
-
-                            // Add Ngo role to the new User
-                            if (result.Succeeded)
-                            {
-                                UserManager.AddToRole(ngo.Id, EAccountType.Ngo.ToString());
-                                await SignInManager.SignInAsync(ngo, isPersistent: false, rememberBrowser: false);
-
-                                return RedirectToAction("Index", "Home");
-                            }
-                            AddErrors(result);
-                        }
-                        break;
+                    return RedirectToAction("Index", "Home");
                 }
 
             }
-
             // If we got this far, something failed, redisplay form
             return View(model);
         }
+
+
+
+
+
+        //===========================//
+        //--------- HELPERS ---------
+        //===========================//
 
         private void AddErrors(IdentityResult result)
         {
@@ -194,11 +137,6 @@ namespace Volunteering.UI.Controllers
                 ModelState.AddModelError("", error);
             }
         }
-
-        //===========================//
-        //--------- HELPERS ---------
-        //===========================//
-
 
         private ActionResult RedirectToLocal(string returnUrl)
         {
@@ -209,6 +147,14 @@ namespace Volunteering.UI.Controllers
             return RedirectToAction("Index", "Home");
         }
 
+
+
+
+
+
+
+
+
         // FOR REFERENCE
         // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
         // Send an email with this link
@@ -216,6 +162,47 @@ namespace Volunteering.UI.Controllers
         // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
         // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
+        //// Switch on Selected Account type
+        //switch (model.AccountType)
+        //{
+        //    // Volunteer Account type selected:
+        //    case EAccountType.Volunteer:
+        //        {
+        //            // create new volunteer and map form values to the instance
+        //            Volunteer v = new Volunteer { UserName = model.Email, Email = model.Email };
+        //            result = await _userService.UserManager.CreateAsync(v, model.Password);
 
+        //            // Add volunteer role to the new User
+        //            if (result.Succeeded)
+        //            {
+        //                _userService.UserManager.AddToRole(v.Id, EAccountType.Volunteer.ToString());
+        //                await _userService.SignInManager.SignInAsync(v, isPersistent: false, rememberBrowser: false);
+        //                // Email confirmation here
+
+        //                return RedirectToAction("Index", "Home");
+        //            }
+        //            AddErrors(result);
+        //        }
+        //        break;
+
+        //    // Ngo Account type selected:
+        //    case EAccountType.Ngo:
+        //        {
+        //            // create new Ngo and map form values to the instance
+        //            Ngo ngo = new Ngo { UserName = model.Email, Email = model.Email };
+        //            result = await _userService.UserManager.CreateAsync(ngo, model.Password);
+
+        //            // Add Ngo role to the new User
+        //            if (result.Succeeded)
+        //            {
+        //                _userService.UserManager.AddToRole(ngo.Id, EAccountType.Ngo.ToString());
+        //                await _userService.SignInManager.SignInAsync(ngo, isPersistent: false, rememberBrowser: false);
+
+        //                return RedirectToAction("Index", "Home");
+        //            }
+        //            AddErrors(result);
+        //        }
+        //        break;
+        //}
     }
 }
