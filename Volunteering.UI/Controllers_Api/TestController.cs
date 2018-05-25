a3646b38c4530c68048b7eb7a6c0af7b7cbe6bfb
Aziz Marzouki <mohamedaziz.marzouki@esprit.tn>
2018-05-25 06:28:52 +0100

    added basic web api support

---
+++ Volunteering.UI/Controllers_Api/TestController.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Web.Http;
+using Volunteering.Service.Identity;
+
+namespace Volunteering.UI.Controllers_Api
+{
+    [RoutePrefix("api/test")]
+    public class TestController : ApiController
+    {
+        private UserService us = new UserService();
+
+        [Route("getValues")]
+        // GET: api/Test
+        public IQueryable<object> Get()
+        {
+            var users = us.UserManager.Users.Select(u => new { UserName = u.UserName, Email = u.Email, Role = u.Roles });
+            return users;
+        }
+
+        // GET: api/Test/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Test
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Test/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Test/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
